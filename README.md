78f9f8e4f5edbe661ad6959bea4b766490a90fbc
NelMatrix743 <nelmatrix155@gmail.com>
2025-07-22 18:43:26 +0000

    chore: update repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# FileTrackZ
+
+![filetrackz logo](https://github.com/NelMatrix743/FileTrackZ/blob/main/assets/filetrackz_logo.png?raw=true "FileTrackZ Logo")
+
--- filetrackz/main.py
+++ filetrackz/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from filetrackz!")
+def main() -> None:
+    print("Hello FileTrackZ.")
 
 
 if __name__ == "__main__":
+
     main()
